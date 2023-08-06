8961fb1f34a7579fb3e3df37a3d21b007aaa41ba
YUU522 <kuromu2750@gmail.com>
2023-08-06 06:59:34 +0000

    [excel17]3-001

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'books' => 'books#books'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit'
-  post 'books' => 'books#create'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
