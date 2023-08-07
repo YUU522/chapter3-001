6ad0e1b433cfd00b9af8457913e568634c34702e
YUU522 <kuromu2750@gmail.com>
2023-08-07 09:47:17 +0000

    [system Complete]3-001

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,26 +7,37 @@ class BooksController < ApplicationController
   
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
+    if @book.save
     # 4. トップ画面へリダイレクト
-    redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      flash.now[:alert] 
+      render :index
+    end
   end
   
   def show
      @book = Book.find(params[:id])  
   end 
-  
+
   def edit
     @book = Book.find(params[:id])
   end
   
-    def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice]="Book was successfully updated."
+    redirect_to book_path(@book.id)  
+    else
+      flash.now[:alert] 
+      render :edit
     end
+  end
     
   def destroy
     book = Book.find(params[:id])
@@ -39,5 +50,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-end
+end  
